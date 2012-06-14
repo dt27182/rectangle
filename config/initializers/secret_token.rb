5c536ce1d6d377ec7c6478219751c6acd8babd47
Danny Tang <dt27182@berkeley.edu>
2012-06-14 16:00:25 -0700

    rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rectangle::Application.config.secret_token = 'c288c88d5106fe0b862ea5f4ae0c1c9c05fe9f8428ea4ff4e74675295c5847524e752c7ace14906a01d9335d585b139c8b4638116c03c85c41c7772f7c930326'
