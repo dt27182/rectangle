4cb8f0f2a7b927d422e8dcc623374125660b153f
dt27182 <dt27182@berkeley.edu>
2012-06-17 15:35:48 -0700

    setup initial routes

--- Gemfile
+++ Gemfile
@@ -32,8 +32,8 @@ end
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
-  gem 'sass-rails', "  ~> 3.1.0"
-  gem 'coffee-rails', "~> 3.1.0"
+  gem 'sass-rails'
+  gem 'coffee-rails'
   gem 'uglifier'
   gem 'therubyracer'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,57 +2,19 @@ Rectangle::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+	#homepage
+	match '/' => 'index#index', :as => index
+	#website listings page for main category
+	match '/categories/:name/show', :as => 'categories#show'
+	#wesite listings page for sub category
+	match '/categories/:name/subcategories/:subname/show', :as => 'subcategories#show'
+	#page for website description and comments
+	match '/website/:id/show', :as => 'websites#show'
+	#page to create review for website
+	match '/website/:id/comments/new', :as => 'comments#new'
+	#page to create reply for a existing comment
+	match '/comment/:id/comments/new', :as => 'comments#newreply'
+	#page to input new website
+	match '/websites/new', :as => 'websites#new'
+  
 end
