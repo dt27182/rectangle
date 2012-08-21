88d54e77becf91f3db883c896abbba77ad1305fa
dt27182 <dt27182@berkeley.edu>
2012-08-20 19:39:16 -0700

    added users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rectangle::Application.routes.draw do
   devise_for :users
-
+	root :to => 'index#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120812063051) do
+ActiveRecord::Schema.define(:version => 20120820231208) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -30,6 +30,11 @@ ActiveRecord::Schema.define(:version => 20120812063051) do
 
   create_table "communities", :force => true do |t|
     t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "endorsements", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
@@ -69,6 +74,11 @@ ActiveRecord::Schema.define(:version => 20120812063051) do
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+
+  create_table "votes", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "websites", :force => true do |t|
     t.string   "name"
